6b9baf60ee3daabf517ad7e44d3950830efbc3ec
Architmaurya <architmaurya7054@gmail.com>
2025-11-19 15:08:44 +0530

    add

--- url-backend/server.js
+++ url-backend/server.js
@@ -17,7 +17,7 @@ connectDB();
 app.use("/", urlRoutes);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () =>
-  console.log(`🚀 Server running at http://localhost:${PORT}`)
+  console.log(`🚀 Server running at http://localhost:`)
 );
