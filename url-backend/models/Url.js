a74af4c38186d50ae30d8ed46245143feb2523ba
Architmaurya <architmaurya7054@gmail.com>
2025-11-19 15:39:24 +0530

    fix

--- url-backend/models/Url.js
+++ url-backend/models/Url.js
@@ -3,13 +3,12 @@ import mongoose from "mongoose";
 const urlSchema = new mongoose.Schema({
   shortId: { type: String, required: true, unique: true },
   longUrl: { type: String, required: true },
-  cryptoDomain: { type: String, required: true },
   clicks: { type: Number, default: 0 },
 
   createdAt: {
     type: Date,
     default: Date.now,
-    expires: 180
+    expires: 180 // auto delete after 3 minutes
   }
 });
 
